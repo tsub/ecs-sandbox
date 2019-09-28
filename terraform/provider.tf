a35febb110b183394fc4816e9d09e8dc1b9d9f91
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2019-09-28 12:58:24 +0900

    feat: Create VPC and Subnets, ...etc

---
+++ terraform/provider.tf
@@ -0,0 +1,4 @@
+provider "aws" {
+  version = "~> 2.30.0"
+  region  = "ap-northeast-1"
+}
---
+++ terraform/vpc.tf
@@ -0,0 +1,19 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.15.0"
+
+  name = "tsub-sandbox"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-northeast-1a", "ap-northeast-1c", "ap-northeast-1d"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
