b8c616ef321686fe7be1aa1cea606445a5fde265
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2019-10-05 15:58:02 +0900

    feat: Manage automatically created log groups of CloudWatch Logs

--- terraform/cwl.tf
+++ terraform/cwl.tf
@@ -12,3 +12,18 @@ resource "aws_cloudwatch_log_stream" "firelens" {
   name           = "firelens"
   log_group_name = aws_cloudwatch_log_group.firehose.name
 }
+
+resource "aws_cloudwatch_log_group" "codebuild-deployment" {
+  name              = "/aws/codebuild/${aws_codebuild_project.deployment.name}"
+  retention_in_days = local.log_retention_in_days
+}
+
+resource "aws_cloudwatch_log_group" "ecs-container-insights" {
+  name              = "/aws/ecs/containerinsights/${aws_ecs_cluster.main.name}/performance"
+  retention_in_days = 1 # fixed from ECS
+}
+
+resource "aws_cloudwatch_log_group" "lambda-json-parse" {
+  name              = "/aws/lambda/${aws_lambda_function.json-parse.function_name}"
+  retention_in_days = local.log_retention_in_days
+}
